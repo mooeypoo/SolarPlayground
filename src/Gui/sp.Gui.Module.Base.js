0e00c887b58031c620dfe984684a96d40e64bfd5
Moriel Schottlender <moriel@gmail.com>
2014-06-14 14:24:53 -0400

    Creating a gui module basey

---
+++ src/Gui/sp.Gui.Module.Base.js
@@ -0,0 +1,72 @@
+/**
+ * General Gui module
+ *
+ * @class sp.Gui.Module.Base
+ * @abstract
+ *
+ * @param {jQuery} $container The container to attach the GUI to
+ * @param {Object} [config] Gui module definition
+ */
+sp.Gui.Module.Base = function SpGuiModuleOoui ( $container, config ) {
+	config = config || {};
+
+	// Mixin constructors
+	OO.EventEmitter.call( this );
+
+	this.$container = $container;
+
+	this.scenario = null;
+};
+
+/* Inheritance */
+OO.mixinClass( sp.Gui.Module.Base, OO.EventEmitter );
+
+/* Events */
+
+/**
+ * Play or pause scenario
+ * @event play
+ * @param {boolean} isPlay Play scenario or pause
+ */
+
+/**
+ * Zoom in or out
+ * @event zoom
+ * @param {number} zoomLevel How much to zoom. Negative to zoom out.
+ */
+
+/**
+ * Change point of view object
+ * @event pov
+ * @param {string} povObjName New POV object name or key
+ */
+
+/* Methods */
+
+/**
+ * Connect the GUI to the scenario it controls
+ * @param {sp.Scenario} scenario The scenario object this GUI controls
+ */
+sp.Gui.Module.Base.prototype.setScenario = function ( scenario ) {
+	this.scenario = scenario;
+};
+
+/**
+ * Initialize the Gui
+ * @abstract
+ * @returns {OO.ui.Toolbar}
+ */
+sp.Gui.Module.Base.prototype.initialize = function () {
+	throw new Error( 'sp.Gui.Module.Initialize must be implemented in child class.' );
+};
+
+/**
+ * Add a tool to the POV list
+ * @abstract
+ * @param {string} name Tool name
+ * @param {string} title Title or alternate text
+ * @param {string} [icon] Tool icon
+ */
+sp.Gui.Module.Base.prototype.addToPOVList = function ( name, title, icon ) {
+	throw new Error( 'sp.Gui.Module.addToPOVList must be implemented in child class.' );
+};
